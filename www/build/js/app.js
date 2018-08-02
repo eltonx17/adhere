7ccc2f997d5b8c15fc7b99fb15705dfb40c8e54a
Nidhin <nidhin@152hq.com>
2018-08-02 13:37:50 +1000

    gulp build

--- www/build/js/app.js
+++ www/build/js/app.js
@@ -54,104 +54,6 @@
 
 
 })();
-//set global configuration of application and it can be accessed by injecting appConstants in any modules
-
-(function () {
-    'use strict';
-
-    angular.module('adhere')
-        .service('appConfig', appConfig);
-
-    /** @ngInject */
-    function appConfig() {
-        var $self = this;
-
-        $self.title = "Ad-built Plus"; // app name     
-        $self.version = "1.0.0";
-        $self.baseURL = 'services/'; // app service URL  
-        $self.requestURL = {
-            login: 'login/login.php'
-        };
-
-    }
-})();
-(function () {
-    apiService.$inject = ["$rootScope", "$http", "$q", "$state", "appConfig", "$mdToast", "$document", "$rootScope", "$timeout"];
-    angular
-        .module('adhere')
-        .service('apiService', apiService);
-
-    function apiService($rootScope, $http, $q, $state, appConfig, $mdToast, $document, $rootScope, $timeout) {
-
-        var $self = this;
-
-        /**
-         * function to place http request
-         */
-        $self.serviceRequest = function (config, success, fail) {
-
-            var requestParams = angular.merge({
-
-                method: config.method || "GET",
-                url: appConfig.baseURL + config.url,
-                params: config.params || {},
-                data: config.data || {}
-            }, config.addOns);
-
-            var request = $http(requestParams);
-
-            request.then(function successCallback(response) {
-                if (response && response.status == 200) {
-                    if (success)
-                        success(response.data);
-                    else {
-                        if (fail)
-                            fail(response.data);
-                    }
-                } else {
-                    if (fail)
-                        fail(response.data);
-                }
-            }, function errorCallback(response) {
-                if (fail)
-                    fail(response.data);
-            });
-        };
-
-        /**
-         * function to place async service request
-         */
-        $self.asyncServiceRequest = function (params) {
-            var deferred = $q.defer(); // creating the promise object
-
-            serviceRequest(params, function (response) {
-                deferred.resolve(response); // resolving the promise
-            }, function (response) {
-                deferred.reject(response); // rejecting the promise
-            });
-
-            return deferred.promise; // returning the promise object
-        };
-
-        /**
-         * to toasts to the user
-         */
-        $self.toast = function (text, param) {
-            $mdToast.show(
-                $mdToast.simple()
-                .textContent(text || 'Take2')
-                .hideDelay(1500)
-            );
-        };
-
-        /**
-         * function to log user out and clear session settings
-         */
-        $self.logout = function (param) {
-            $state.go('login'); // navigate to login
-        };
-    }
-})();
 /**
  * Adhere
  **/
@@ -306,6 +208,149 @@
     }
 
 })();
+//set global configuration of application and it can be accessed by injecting appConstants in any modules
+
+(function () {
+    'use strict';
+
+    angular.module('adhere')
+        .service('appConfig', appConfig);
+
+    /** @ngInject */
+    function appConfig() {
+        var $self = this;
+
+        $self.title = "Ad-built Plus"; // app name     
+        $self.version = "1.0.0";
+        $self.baseURL = 'services/'; // app service URL  
+        $self.requestURL = {
+            login: 'login/login.php'
+        };
+
+    }
+})();
+(function () {
+    apiService.$inject = ["$rootScope", "$http", "$q", "$state", "appConfig", "$mdToast", "$document", "$rootScope", "$timeout"];
+    angular
+        .module('adhere')
+        .service('apiService', apiService);
+
+    function apiService($rootScope, $http, $q, $state, appConfig, $mdToast, $document, $rootScope, $timeout) {
+
+        var $self = this;
+
+        /**
+         * function to place http request
+         */
+        $self.serviceRequest = function (config, success, fail) {
+
+            var requestParams = angular.merge({
+
+                method: config.method || "GET",
+                url: appConfig.baseURL + config.url,
+                params: config.params || {},
+                data: config.data || {}
+            }, config.addOns);
+
+            var request = $http(requestParams);
+
+            request.then(function successCallback(response) {
+                if (response && response.status == 200) {
+                    if (success)
+                        success(response.data);
+                    else {
+                        if (fail)
+                            fail(response.data);
+                    }
+                } else {
+                    if (fail)
+                        fail(response.data);
+                }
+            }, function errorCallback(response) {
+                if (fail)
+                    fail(response.data);
+            });
+        };
+
+        /**
+         * function to place async service request
+         */
+        $self.asyncServiceRequest = function (params) {
+            var deferred = $q.defer(); // creating the promise object
+
+            serviceRequest(params, function (response) {
+                deferred.resolve(response); // resolving the promise
+            }, function (response) {
+                deferred.reject(response); // rejecting the promise
+            });
+
+            return deferred.promise; // returning the promise object
+        };
+
+        /**
+         * to toasts to the user
+         */
+        $self.toast = function (text, param) {
+            $mdToast.show(
+                $mdToast.simple()
+                .textContent(text || 'Take2')
+                .hideDelay(1500)
+            );
+        };
+
+        /**
+         * function to log user out and clear session settings
+         */
+        $self.logout = function (param) {
+            $state.go('login'); // navigate to login
+        };
+    }
+})();
+/**
+ * Adhere
+ **/
+(function () {
+    "use strict";
+
+    homeController.$inject = ["$scope", "appConfig", "$timeout", "apiService", "$rootScope", "$state"];
+    angular
+        .module('adhere')
+        .controller('homeController', homeController);
+
+    /* ngInject */
+    function homeController($scope, appConfig, $timeout, apiService, $rootScope, $state) {
+        var vm = this;
+
+
+        function init() {
+            vm.appTitle = appConfig.title; // binds app title from config
+            vm.getComments();
+        };
+
+        vm.getComments = function () {
+            vm.commentList = [{
+                comment: "This is a test comment from Nidhin",
+                from: "Nidhin",
+                pic: "",
+                time: new Date(),
+                id: "123"
+            }, {
+                comment: "This is a test comment from Sourabh",
+                from: "Sourabh",
+                pic: "",
+                time: new Date(),
+                id: "864"
+            }];
+        };
+        vm.showReplySection = function (id) {
+            $("#comment-reply-" + id).slideToggle();
+        }
+
+        init();
+
+    }
+
+})();
 /**
  * Author:Adhere
  **/
@@ -426,51 +471,6 @@
                 active:  false
             }]
         };
-
-        init();
-
-    }
-
-})();
-/**
- * Adhere
- **/
-(function () {
-    "use strict";
-
-    homeController.$inject = ["$scope", "appConfig", "$timeout", "apiService", "$rootScope", "$state"];
-    angular
-        .module('adhere')
-        .controller('homeController', homeController);
-
-    /* ngInject */
-    function homeController($scope, appConfig, $timeout, apiService, $rootScope, $state) {
-        var vm = this;
-
-
-        function init() {
-            vm.appTitle = appConfig.title; // binds app title from config
-            vm.getComments();
-        };
-
-        vm.getComments = function () {
-            vm.commentList = [{
-                comment: "This is a test comment from Nidhin",
-                from: "Nidhin",
-                pic: "",
-                time: new Date(),
-                id: "123"
-            }, {
-                comment: "This is a test comment from Sourabh",
-                from: "Sourabh",
-                pic: "",
-                time: new Date(),
-                id: "864"
-            }];
-        };
-        vm.showReplySection = function (id) {
-            $("#comment-reply-" + id).slideToggle();
-        }
 
         init();
 
