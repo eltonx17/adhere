3d3cfc163803d4d35bade48320ff3c8a51d3c678
Elton Gonsalves <eltonx17@gmail.com>
2018-08-04 19:20:43 +1000

    Create mentor mapping table

--- webapp/services/create.sql
+++ webapp/services/create.sql
@@ -1,3 +1,4 @@
+-- Create 'Users' Table
 CREATE TABLE IF NOT EXISTS `users` (
   `uid` int(11) NOT NULL AUTO_INCREMENT,
   `firstname` varchar(100) NOT NULL,
@@ -9,3 +10,12 @@ CREATE TABLE IF NOT EXISTS `users` (
   PRIMARY KEY (`uid`),
   UNIQUE KEY `email_UNIQUE` (`email`)
 ) ENGINE=MyISAM AUTO_INCREMENT=19 DEFAULT CHARSET=latin1;
+
+-- Create 'mentormapping' Table
+CREATE TABLE IF NOT EXISTS `mentormapping` (
+  `mapid` int(11) NOT NULL AUTO_INCREMENT,
+  `menteeid` int(11) DEFAULT NULL,
+  `mentorid` int(11) DEFAULT NULL,
+  PRIMARY KEY (`mapid`),
+  KEY `menteeid_idx` (`menteeid`)
+) ENGINE=MyISAM AUTO_INCREMENT=6 DEFAULT CHARSET=latin1 COMMENT='Mentor Mapping - Connecting Mentee with Mentor';
