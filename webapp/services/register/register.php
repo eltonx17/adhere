dca8a1bd676b1894844c6891b2f71dc8637c2079
Elton Gonsalves <eltonx17@gmail.com>
2018-08-04 19:23:16 +1000

    mentor mapping in registration

--- webapp/services/register/register.php
+++ webapp/services/register/register.php
@@ -7,7 +7,6 @@ $lastname = "";
 $email    = "";
 $usertype = "";
 $mentorid = "";
-$errors = array(); 
 
 //db connection string
 $db = new mysqli("localhost", "root", "", "adhere") or die($this->db->error);
@@ -20,21 +19,29 @@ $email = mysqli_real_escape_string($db, $_GET['email']);
 $password_1 = mysqli_real_escape_string($db, $_GET['password']);
 $usertype = mysqli_real_escape_string($db, $_GET['userType']);
     
-//encrypt the password before saving in the database
+//Encrypt the password before saving in the database
 $password = md5($password_1);
 
 //Query to register user info into db
 $query = "INSERT INTO users (firstname, lastname, email, password, usertype) VALUES('$firstname', '$lastname', '$email', '$password', '$usertype')";
 mysqli_query($db, $query);
 
-//Check if the query is valid and echo accordingly
+//Retrieve the ID of the inserted record
+$last_uid = $db->insert_id;
+
+//Check if the query is valid, if yes check for usertype=2 i.e mentee
 if (!$query) {
-        array_push($errors, "Error inserting into database");
-        echo json_encode($errors);
+     $error = array(
+            'data'=>'null', 'error'=>array('msg'=>'Error','code'=>'46')
+            );
+        echo json_encode($error);
     }
     else{
-        array_push($errors, "Success");
-        echo json_encode($errors);
+        //If Mentee, fire another query to map mentee to a mentor
+        if ($usertype == 2){
+            $mentorid = mysqli_real_escape_string($db, $_GET['mentorId']);
+            $mapquery = "INSERT INTO mentormapping (menteeid, mentorid) VALUES ('$last_uid', '$mentorid')";
+            mysqli_query($db, $mapquery);
+       } 
     }
-  	
 ?>
