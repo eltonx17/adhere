c60b941ff8b783058b0344794fabbb00df3fd085
Nidhin <nidhin@152hq.com>
2018-08-11 22:38:14 +1000

    toggleStatus.php bug fixes

--- webapp/services/admin/toggleStatus.php
+++ webapp/services/admin/toggleStatus.php
@@ -3,12 +3,18 @@ header('Content-type: application/json');
 require "../conn.php";
 
 $userId = mysqli_real_escape_string($db, $_GET['userID']);
-$action = mysqli_real_escape_string($db, $_GET['action'])
-    
-if($action==1){
+$action = mysqli_real_escape_string($db, $_GET['action']);
+
+if(($action != "0") && ($action != "1")){   
+     $error = array(
+                 'data'=>0, 'error'=>array('msg'=>'Failed to update status','code'=>'401')
+                 );
+    return;
+}
+
     $query="UPDATE users
-            SET userstatus = 0
-            WHERE uid = '$userId';"
+            SET userstatus = '$action'
+            WHERE uid = '$userId'";
     if(!$query){
         $error = array(
                  'data'=>0, 'error'=>array('msg'=>'Failed to update status','code'=>'401')
@@ -20,22 +26,6 @@ if($action==1){
                     );
         echo json_encode($success);
     }
-}
-else-if($action==0){
-    $query="UPDATE users
-            SET userstatus = 1
-            WHERE uid = '$userId';"
-    if(!$query){
-        $error = array(
-                 'data'=>0, 'error'=>array('msg'=>'Failed to update status','code'=>'401')
-                 );
-    }
-    else{
-        $success = array(
-                   'data'=>'User status updated successfully', 'error'=>null
-                    );
-        echo json_encode($success);
-    }
-}
+
 
 ?>
