d53a6b2d66ba551e148e0a389b3d81822d246897
Elton Gonsalves <rockr172@gmail.com>
2018-08-11 20:53:23 +1000

    Add update.sql and make folders for user roles

--- webapp/services/create.sql
+++ webapp/services/create.sql
@@ -6,16 +6,20 @@ CREATE TABLE IF NOT EXISTS `users` (
   `email` varchar(100) NOT NULL,
   `password` varchar(100) NOT NULL,
   `usertype` int(1) DEFAULT NULL COMMENT '0 - Admin 1 - Mentor 2 - Mentee',
-  `userstatus` int(1) DEFAULT '0' COMMENT '0 - Inactive 1 - Under Review 2 - Verified',
+  `accountstatus` int(1) DEFAULT '0' COMMENT '0 - Inactive 1 - Active',
   PRIMARY KEY (`uid`),
   UNIQUE KEY `email_UNIQUE` (`email`)
-) ENGINE=MyISAM AUTO_INCREMENT=28 DEFAULT CHARSET=latin1;
+) ENGINE=MyISAM AUTO_INCREMENT=26 DEFAULT CHARSET=latin1;
+
 
 -- Create 'mentormapping' Table
 CREATE TABLE IF NOT EXISTS `mentormapping` (
-  `mapid` int(11) NOT NULL AUTO_INCREMENT,
-  `menteeid` int(11) DEFAULT NULL,
+  `idmentormapping` int(11) NOT NULL AUTO_INCREMENT,
   `mentorid` int(11) DEFAULT NULL,
-  PRIMARY KEY (`mapid`),
-  KEY `menteeid_idx` (`menteeid`)
-) ENGINE=MyISAM AUTO_INCREMENT=6 DEFAULT CHARSET=latin1 COMMENT='Mentor Mapping - Connecting Mentee with Mentor';
+  `menteeid` int(11) DEFAULT NULL,
+  `mapstatus` int(11) DEFAULT '0' COMMENT '0 - Under Review \n1 - Accepted\n2 - Rejected',
+  PRIMARY KEY (`idmentormapping`),
+  UNIQUE KEY `menteeid_UNIQUE` (`menteeid`),
+  KEY `mentorID_idx` (`mentorid`),
+  KEY `menteeFK_idx` (`menteeid`)
+) ENGINE=MyISAM AUTO_INCREMENT=5 DEFAULT CHARSET=latin1;
