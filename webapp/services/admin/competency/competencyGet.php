084f3057e6b36d1236fcde24ff71cc4c3a0ec672
Elton Gonsalves <rockr172@gmail.com>
2018-08-30 15:48:38 +1000

    Retrieve competency data from the table

---
+++ webapp/services/admin/competency/competencyGet.php
@@ -0,0 +1,22 @@
+<?php
+header('Content-type: application/json');
+require "../../conn.php";
+
+$compList = "";
+$compArray = array();
+
+$query = "SELECT * FROM competency ORDER BY timestamp DESC LIMIT 1";
+$executeQuery = mysqli_query($db, $query);
+
+if($executeQuery){
+    while($compList = mysqli_fetch_assoc($executeQuery)) {
+        $compData[] = $compList;       
+    }
+    echo json_encode($compData);
+} else {
+    $error = array(
+            'data'=>'null', 'error'=>array('msg'=>'Error retrieving competency data','code'=>'102')
+        );
+        echo json_encode($error);
+}
+?>
