3e8f496fd63683a5ca63b5065f08bd222f5f7e7c
Sourabh Kulahalli <sourabhck@gmail.com>
2018-08-30 18:36:56 +1000

    Mentee Stage Details

---
+++ webapp/services/admin/competency/menteeStageDetails.php
@@ -0,0 +1,35 @@
+<?php
+header('Content-type: application/json');
+
+require "../conn.php";
+
+$stageData = "";
+
+//Get menteeID
+$menteeId = mysqli_real_escape_string($db, $_GET['menteeID']);
+
+//Query to get mentee stage details
+$query = "SELECT menteeworkbook.menteeid,menteeworkbook.gst,menteeworkbook.stage1,menteeworkbook.stage2,menteeworkbook.stage3,menteeworkbook.stage4,menteeworkbook.stage5 FROM menteeworkbook WHERE menteeworkbook.menteeid = '$menteeId'";
+$executeQuery = mysqli_query($db, $query);
+
+if($executeQuery){
+    $stageData = mysqli_fetch_assoc($executeQuery);
+                             
+    $stageData["stage1"] = json_decode($stageData['stage1']);
+    $stageData["stage2"] = json_decode($stageData['stage2']);    
+    $stageData["stage3"] = json_decode($stageData['stage3']);
+    $stageData["stage4"] = json_decode($stageData['stage4']);
+    $stageData["stage5"] = json_decode($stageData['stage5']);
+    $stageData["gst"] = $stageData['gst'];
+    
+    echo json_encode($stageData);    
+
+} else {
+    $error = array(
+            'data'=>'null', 'error'=>array('msg'=>'Error retrieving competency data','code'=>'102')
+        );
+        echo json_encode($error);
+}
+
+
+?>
