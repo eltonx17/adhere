52592e624ab466b161424c3ece3a4634b2de66d2
Elton Gonsalves <rockr172@gmail.com>
2018-08-30 16:22:34 +1000

    Scrap while loop, unnecessary when retrieving only one value

--- webapp/services/admin/competency/competencyGet.php
+++ webapp/services/admin/competency/competencyGet.php
@@ -9,10 +9,8 @@ $query = "SELECT * FROM competency ORDER BY timestamp DESC LIMIT 1";
 $executeQuery = mysqli_query($db, $query);
 
 if($executeQuery){
-    while($compList = mysqli_fetch_assoc($executeQuery)) {
-        $compData = $compList;       
-    }
-    echo json_encode($compData);
+    $compList = mysqli_fetch_assoc($executeQuery);
+    echo json_encode($compList);
 } else {
     $error = array(
             'data'=>'null', 'error'=>array('msg'=>'Error retrieving competency data','code'=>'102')
