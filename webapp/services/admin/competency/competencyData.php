936d94b530c03678e300341c2d0d398be1d43935
Nidhin <nidhin@152hq.com>
2018-08-30 16:49:38 +1000

    json_encode

--- webapp/services/admin/competency/competencyData.php
+++ webapp/services/admin/competency/competencyData.php
@@ -7,11 +7,15 @@ $date->setTimeZone(new DateTimeZone('Australia/Sydney'));
 $timestamp = $date->format('Y-m-d H:i:s');
 
 //Retrieve Competency String
-$competencyData = mysqli_real_escape_string($db, $_POST['competencyData']);
+//$competencyData = mysqli_real_escape_string($db, $_POST['competencyData']);
 $postdata = file_get_contents("php://input");
 //$request = json_decode($postdata);
 
 echo $postdata;
+$request = json_decode($postdata);
+
+echo json_encode($request->competencyData);
+return;
 
 $query = "INSERT INTO competency (timestamp, data) VALUES ('$timestamp', '$competencyData')";
 $executeQuery = mysqli_query($db, $query);
