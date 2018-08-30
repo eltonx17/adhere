b4b544fa154988d5f396398f2cbfdf4a1f95b770
Nidhin <nidhin@152hq.com>
2018-08-30 16:43:10 +1000

    competecny data php integration

--- webapp/modules/admin/competency/competency.controller.js
+++ webapp/modules/admin/competency/competency.controller.js
@@ -53,23 +53,11 @@
         /**
          *
          */
-        vm.addQuestion = function (item) {
-            item.questions.push({
-
-            });
-            console.log(angular.toJson(vm.competencyList))
-        };
-        /**
-         *
-         */
-        vm.save = function (item) {
+        vm.currentCompetency = function (item) {
             // sent login request to server
             apiService.serviceRequest({
-                    method: 'POST',
-                    url: appConfig.requestURL.saveCompetencyData,
-                    data: {
-                        competencyData: vm.competencyList
-                    }
+                    method: 'GET',
+                    url: appConfig.requestURL.getCompetencyData
                 }, function (data) {
                     if (data && data.error && data.error.msg) { // error from server                                                      
                         $timeout(function () {
@@ -89,6 +77,58 @@
                     vm.regoErr = true;
                     vm.regoErrMsg = "Something went wrong, try again.";
                 });
+        };
+        /**
+         *
+         */
+        vm.addQuestion = function (item) {
+            item.questions.push({
+
+            });
+            console.log(angular.toJson(vm.competencyList))
+        };
+        /**
+         *
+         */
+        vm.save = function (item) {
+            swal({
+                    title: "Are you sure?",
+                    text: "Saving would overwrite the existing version and this action cannot be reversed.",
+                    icon: "warning",
+                    buttons: true,
+                    dangerMode: true,
+                })
+                .then((willDelete) => {
+                    if (willDelete) {
+                        // sent login request to server
+                        apiService.serviceRequest({
+                                method: 'POST',
+                                url: appConfig.requestURL.saveCompetencyData,
+                                data: {
+                                    competencyData: vm.competencyList
+                                }
+                            }, function (data) {
+                                if (data && data.error && data.error.msg) { // error from server                                                      
+                                    $timeout(function () {
+                                        vm.regoErrMsg = data.error.msg || "Something went wrong, try again.";
+                                    });
+                                } else {
+                                    swal({
+                                        title: "Good job!",
+                                        text: "You clicked the button!",
+                                        icon: "success",
+                                        button: "Aww yiss!",
+                                    });
+                                }
+                            },
+                            function (fail) { // service fails
+                                vm.registering = false;
+                                vm.regoErr = true;
+                                vm.regoErrMsg = "Something went wrong, try again.";
+                            });
+                    }
+                });
+
         };
         init();
 
