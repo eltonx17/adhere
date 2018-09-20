0a62389db57056b77d7a32c277bca407aa8cb248
Elton Gonsalves <rockr172@gmail.com>
2018-09-20 16:24:49 +1000

    Upload API to update evidence table with original filename and filepath

--- webapp/services/admin/competency/upload.php
+++ webapp/services/admin/competency/upload.php
@@ -1,7 +1,12 @@
 <?php
 
+header('Content-type: application/json');
+require "../../conn.php";
+
 $menteeId = $_POST["menteeId"];
-//$menteeId = '24';
+$workbookId = $_POST["workbookId"];
+/*$menteeId = '22';
+$workbookId = '13';*/
 $uploadsDir = "../../../uploads/";
 $directory = "../../../uploads/" . $menteeId . "/";
 $evidence = "../../../uploads/" . $menteeId . "/evidence/";
@@ -19,21 +24,15 @@ else{
         if (!file_exists($directory)) {
             mkdir("../../../uploads/" . $menteeId, 0777);
             mkdir("../../../uploads/" . $menteeId ."/evidence/", 0777);
-            $success = array('data'=>"Directory created successfully ", 'error'=>null);
-            echo json_encode($success);
         }
         if (file_exists($directory) && !file_exists($evidence)) {
             mkdir("../../../uploads/" . $menteeId ."/evidence/", 0777);
-            $success = array('data'=>"Directory evidence was missing, created successfully ", 'error'=>null);
-            echo json_encode($success);
         }
     }
     else{
         mkdir("../../../uploads/", 0777);
         mkdir("../../../uploads/" . $menteeId, 0777);
         mkdir("../../../uploads/" . $menteeId ."/evidence/", 0777);
-        $success = array('data'=>"Directory created successfully ", 'error'=>null);
-        echo json_encode($success);
     }
 }
 
@@ -45,13 +44,15 @@ $date = date_timestamp_get($date);
 // Count # of uploaded files in array
 $total = count($_FILES['upload']['name']);
 $counter = 0;
+$errorCount = 0;
 
 // Loop through each file
 for( $i=0 ; $i < $total ; $i++ ) {
     //Get the temp file path
     $tmpFilePath = $_FILES['upload']['tmp_name'][$i];
     $fName = $_FILES['upload']['name'][$i];
-    
+    $fName = mb_strimwidth($fName, 0, 50, "...");
+
     //extract extension and append it along with the date
     $ext = pathinfo($fName, PATHINFO_EXTENSION);
     $fileName = $date.$i.".".$ext;
@@ -60,21 +61,31 @@ for( $i=0 ; $i < $total ; $i++ ) {
     if ($tmpFilePath != ""){
         //Setup our new file path with new file name
         $newFilePath = $evidence.$fileName;
-
         //Upload the file into the temp dir
         if(move_uploaded_file($tmpFilePath, $newFilePath)) {
-            $counter++;
-        }
-        else{
-            $error = array(
-            'data'=>'null', 'error'=>array('msg'=>'Error uploading files','code'=>'000')
-            );
-            echo json_encode($error);
+            echo $fName;
+            $query = "INSERT INTO evidence (menteeid, workbookid, filepath, filename) 
+                      VALUES('$menteeId','$workbookId','$newFilePath','".base64_encode($fName)."')";        
+            $executeQuery= mysqli_query($db, $query);
+            if(mysqli_affected_rows($db) > 0 ){
+                $counter++;
+            }
+            else{
+                $errorCount++;
+            }
         }
     }
 }
 
-$success = array('data'=>"Successfully uploaded " .$counter. " files", 'error'=>null);
-echo json_encode($success);
+if ($counter>0 && $errorCount==0 ){
+    $success = array('data'=>"Successfully uploaded " .$counter. " files", 'error'=>null);
+    echo json_encode($success);
+}
+else{
+    $error = array(
+    'data'=>'null', 'error'=>array('msg'=>"Error uploading ".$errorCounter." files",'code'=>'000')
+    );
+    echo json_encode($error);
+}
 
 ?>
