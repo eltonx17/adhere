6b27f4bf1b175d8f5e581b0356da3bc3476f1cd4
Nidhin <nidhin@152hq.com>
2018-09-20 14:56:49 +1000

    independent user

--- webapp/modules/mentee/mentee.controller.js
+++ webapp/modules/mentee/mentee.controller.js
@@ -9,7 +9,7 @@
         .controller('menteeController', menteeController);
 
     /* ngInject */
-    function menteeController($scope, appConfig, $timeout, apiService, $rootScope, $state) {
+    function menteeController($scope, appConfig, $timeout, apiService, $rootScope, $state, $mdDialog) {
         var vm = this;
 
 
@@ -17,22 +17,23 @@
             vm.user = window.localStorage.getItem('user') ? angular.fromJson(window.localStorage.getItem('user')) : undefined;
             vm.appTitle = appConfig.title; // binds app title from config
             vm.getComments();
-            vm.stageStatus = {           
-                1 : "Step 1/5",
-                2 : "Completed Step 1/5",
-                3 : "Completed Step 2/5",
-                4 : "Completed Step 3/5",
-                5 : "Completed Step 4/5",
-                6 : "Completed"
+            vm.stageStatus = {
+                1: "Step 1/5",
+                2: "Completed Step 1/5",
+                3: "Completed Step 2/5",
+                4: "Completed Step 3/5",
+                5: "Completed Step 4/5",
+                6: "Completed"
             };
-            vm.stagePercentage = {           
-                1 : 0,
-                2 : 20,
-                3 : 40,
-                4 : 60,
-                5 : 80,
-                6 : 100
+            vm.stagePercentage = {
+                1: 0,
+                2: 20,
+                3: 40,
+                4: 60,
+                5: 80,
+                6: 100
             };
+            vm.showDialog();
         };
 
         vm.getComments = function () {
@@ -53,6 +54,40 @@
         vm.showReplySection = function (id) {
             $("#comment-reply-" + id).slideToggle();
         }
+        /**
+         *
+         */
+        vm.showDialog = function () {
+            var parentEl = angular.element(document.body);
+            $mdDialog.show({
+                parent: parentEl,
+                template: '<md-dialog aria-label="List dialog">' +
+                    '  <md-dialog-content>' +
+                    '    <md-list>' +
+                    '      <md-list-item ng-repeat="item in items">' +
+                    '       <p>Number {{item}}</p>' +
+                    '      ' +
+                    '    </md-list-item></md-list>' +
+                    '  </md-dialog-content>' +
+                    '  <md-dialog-actions>' +
+                    '    <md-button ng-click="closeDialog()" class="md-primary">' +
+                    '      Close Dialog' +
+                    '    </md-button>' +
+                    '  </md-dialog-actions>' +
+                    '</md-dialog>',
+                locals: {
+                    items: $scope.items
+                },
+                controller: DialogController
+            });
+
+            function DialogController($scope, $mdDialog, items) {
+                $scope.items = [1,2,43];
+                $scope.closeDialog = function () {
+                    $mdDialog.hide();
+                }
+            }
+        };
 
         init();
 
